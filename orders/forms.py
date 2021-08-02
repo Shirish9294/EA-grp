cd52e17702f41a80956c0bec85f91a1a9928b167
Shirish9294 <37794995+Shirish9294@users.noreply.github.com>
2021-08-02 17:06:23 -0500

    update

---
+++ orders/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.db import transaction
+from shop.models import *
+
+from .models import Order
+
+
+class OrderCreateForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        # Order.Customer = Customer.objects.get(user=request.user)
+        fields = ['first_name', 'last_name', 'email', 'address',
+                  'postal_code', 'city']
+
+    # @transaction.atomic
+    # def save(self):
+    #
+    #     Order.save()

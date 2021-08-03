6c888d93f5f127ceed33c2abfcff0ca08e362167
Shirish9294 <37794995+Shirish9294@users.noreply.github.com>
2021-08-02 23:11:45 -0500

    Update settings.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -172,7 +172,7 @@ Configuration.configure(
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL ='/media/'

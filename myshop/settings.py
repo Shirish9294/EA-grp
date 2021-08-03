880e153cd91274200b0f82764cf174c4bd117993
Shirish9294 <37794995+Shirish9294@users.noreply.github.com>
2021-08-02 23:14:28 -0500

    Update settings.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -174,7 +174,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL ='/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
